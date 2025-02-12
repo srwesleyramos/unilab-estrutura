487fb12765edb63118d35e30f81461f9526a4ce7
Wesley Gabriel Abreu Ramos <dev.wesleygabriel@gmail.com>
2025-02-12 15:24:42 -0300

    feat: adding first part of exercise

---
+++ 2025-02-12/part_1.cpp
@@ -0,0 +1,125 @@
+#include <stdio.h>
+
+#define LENGTH 10
+
+int main()
+{
+    /*
+     * parte I - Modificando a lista
+     */
+
+    {
+        printf("\n- exemplo 1: zerando todas as posicoes da lista\n\n");
+
+        int vetor[LENGTH] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < LENGTH; i++)
+        {
+            printf("%4d", vetor[i]);
+        }
+
+        printf("\n");
+
+        for (int i = 0; i < LENGTH; i++)
+        {
+            vetor[i] = 0;
+        }
+
+        printf("\n");
+
+        for (int i = 0; i < LENGTH; i++)
+        {
+            printf("%4d", vetor[i]);
+        }
+
+        printf("\n");
+    }
+
+    {
+        printf("\n- exemplo 2: zerando as posicoes pares\n\n");
+
+        int vetor[LENGTH] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < LENGTH; i++)
+        {
+            printf("%4d", vetor[i]);
+        }
+
+        printf("\n");
+
+        for (int i = 0; i < LENGTH; i += 2)
+        {
+            vetor[i] = 0;
+        }
+
+        printf("\n");
+
+        for (int i = 0; i < LENGTH; i++)
+        {
+            printf("%4d", vetor[i]);
+        }
+
+        printf("\n");
+    }
+
+    {
+        printf("\n- exemplo 3: duplicando os elementos impares da lista\n\n");
+
+        int vetor[LENGTH] = {92, 45, 13, 27, 72, 31, 54, 29, 3, 7};
+
+        for (int i = 0; i < LENGTH; i++)
+        {
+            printf("%4d", vetor[i]);
+        }
+
+        printf("\n");
+
+        for (int i = 1; i < LENGTH; i++)
+        {
+            if (vetor[i] % 2 == 1)
+            {
+                vetor[i] *= 2;
+            }
+        }
+
+        printf("\n");
+
+        for (int i = 0; i < LENGTH; i++)
+        {
+            printf("%4d", vetor[i]);
+        }
+
+        printf("\n");
+    }
+
+    {
+        printf("\n- exemplo 4: zerando as duplicatas do numero k\n\n");
+
+        int vetor[LENGTH] = {92, 6, 13, 27, 6, 31, 54, 29, 6, 6};
+        int k = 3;
+
+        for (int i = 0; i < LENGTH; i++)
+        {
+            printf("%4d", vetor[i]);
+        }
+
+        printf("\n");
+
+        for (int i = 0; i < LENGTH; i++)
+        {
+            if (vetor[i] == (k * 2))
+            {
+                vetor[i] = 0;
+            }
+        }
+
+        printf("\n");
+
+        for (int i = 0; i < LENGTH; i++)
+        {
+            printf("%4d", vetor[i]);
+        }
+
+        printf("\n");
+    }
+}
